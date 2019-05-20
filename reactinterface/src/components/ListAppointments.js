81cb87d413840efc0b5af86b2d5e4e69fd5d3c40
koad7 <kodjo.adanledji@weforum.org>
2019-05-20 13:17:34 +0200

    Final

--- reactinterface/src/components/ListAppointments.js
+++ reactinterface/src/components/ListAppointments.js
@@ -1,45 +1,80 @@
 import React, { Component } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import Moment from 'react-moment';
-export default class ListAppointments extends Component {
-  render() {
-    const listItems = this.props.appointments.map(item =>(
-        <div>
-          <div>{item.petName}</div>
-          <div>{item.ownerName}</div>
-        </div>
-      ));
 
+class ListAppointments extends Component {
+  render() {
     return (
-        <div className="appointment-list item-list mb-3" >
-            {this.props.appointments.map(item => (
-                <div className="pet-item col media py-3" key={item.aptId}>
-                <div className="mr-3">
-                  <button className="pet-delete btn btn-sm btn-danger" onClick={() => this.props.deleteAppointment(item)}><FaTimes /></button>
-                </div>
-      
-                <div className="pet-info media-body">
-                  <div className="pet-head d-flex">
-                    <span className="pet-name">{item.aptId}- {item.petName}</span>
-                    <span className="apt-date ml-auto">
-                    <Moment
+      <div className="appointment-list item-list mb-3">
+        {this.props.appointments.map(item => (
+          <div className="pet-item col media py-3" key={item.aptId}>
+            <div className="mr-3">
+              <button
+                className="pet-delete btn btn-sm btn-danger"
+                onClick={() => this.props.deleteAppointment(item)}>
+                <FaTimes />
+              </button>
+            </div>
+
+            <div className="pet-info media-body">
+              <div className="pet-head d-flex">
+                <span
+                  className="pet-name"
+                  contentEditable="true"
+                  suppressContentEditableWarning
+                  onBlur={e =>
+                    this.props.updateInfo(
+                      'petName',
+                      e.target.innerText,
+                      item.aptId
+                    )
+                  }>
+                  
+                  {item.petName}
+                </span>
+                <span className="apt-date ml-auto">
+                  <Moment
                     date={item.aptDate}
-                    parse="YYYY-YY-dd hh:mm"
+                    parse="YYYY-MM-dd hh:mm"
                     format="MMM-D h:mma"
-                    />
-                    </span>
-                  </div>
-      
-                  <div className="owner-name">
-                    <span className="label-item">Owner: </span>
-                    <span>{item.ownerName}</span>
-                  </div>
-                  <div className="apt-notes">aptNotes</div>
-                </div>
+                  />
+                </span>
               </div>
-          
-            ))}
-        </div>        
-    )
+
+              <div className="owner-name">
+                <span className="label-item">Owner: </span>
+                <span
+                  contentEditable="true"
+                  suppressContentEditableWarning
+                  onBlur={e =>
+                    this.props.updateInfo(
+                      'ownerName',
+                      e.target.innerText,
+                      item.aptId
+                    )
+                  }>
+                  {item.ownerName}
+                </span>
+              </div>
+              <div
+                className="apt-notes"
+                contentEditable="true"
+                suppressContentEditableWarning
+                onBlur={e =>
+                  this.props.updateInfo(
+                    'aptNotes',
+                    e.target.innerText,
+                    item.aptId
+                  )
+                }>
+                {item.aptNotes}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
+
+export default ListAppointments;
